fe05c033e559aefd52fcecd11d7ff4699eab6140
carlotagadel <124460669+carlotagadel@users.noreply.github.com>
2023-05-28 18:49:07 +0200

    changes4

--- exito.php
+++ exito.php
@@ -2,20 +2,6 @@
 <html>
 <head>
   <title>Éxito</title>
-
-  <script>
-    function mostrarDatos() {
-      var tabla = document.getElementById("tablaDatos");
-      tabla.style.display = "block";
-    }
-    
-  </script>
-
-  <style>
-    #tablaDatos {
-      display: none;
-    }
-  </style>
 </head>
 
 <body>
@@ -28,50 +14,37 @@
   $password = "";
   $dbname = "cursosql";
 
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
-    $apellido1 = isset($_POST['apellido1']) ? $_POST['apellido1'] : '';
-    $apellido2 = isset($_POST['apellido2']) ? $_POST['apellido2'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $login = isset($_POST['login']) ? $_POST['login'] : '';
-    $pass = isset($_POST['pass']) ? $_POST['pass'] : '';
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-      die("Error en la conexión a la base de datos: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT * FROM usuarios_formulario";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-      echo "<h2>Datos registrados</h2>";
-      echo "<button onclick=\"mostrarDatos()\">Mostrar datos</button>";
-      echo "<table id=\"tablaDatos\">";
-      echo "<tr><th>Nombre</th><th>Apellido1</th><th>Apellido2</th><th>Email</th><th>Login</th><th>Contraseña</th></tr>";
-
-      while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row["nombre"] . "</td>";
-        echo "<td>" . $row["apellido1"] . "</td>";
-        echo "<td>" . $row["apellido2"] . "</td>";
-        echo "<td>" . $row["email"] . "</td>";
-        echo "<td>" . $row["login"] . "</td>";
-        echo "<td>" . $row["pass"] . "</td>";
-        echo "</tr>";
-      }
-      echo "</table>";
-
-    } else {
-
-      echo "No se encontraron datos registrados.";
-
-    }
-
-    $conn->close();
+  if ($conn->connect_error) {
+    die("Error en la conexión a la base de datos: " . $conn->connect_error);
   }
 
+  $sql = "SELECT * FROM usuarios_formulario";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    echo "<h2>Datos registrados</h2>";
+    echo "<table>";
+    echo "<tr><th>Nombre</th><th>Apellido1</th><th>Apellido2</th><th>Email</th><th>Login</th><th>Contraseña</th></tr>";
+
+    while ($row = $result->fetch_assoc()) {
+      echo "<tr>";
+      echo "<td>" . $row["NOMBRE"] . "</td>";
+      echo "<td>" . $row["PRIMER_APELLIDO"] . "</td>";
+      echo "<td>" . $row["SEGUNDO_APELLIDO"] . "</td>";
+      echo "<td>" . $row["EMAIL"] . "</td>";
+      echo "<td>" . $row["LOG_IN"] . "</td>";
+      echo "<td>" . $row["CONTRASEÑA"] . "</td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+
+  } else {
+    echo "<p>No se encontraron datos registrados.</p>";
+  }
+
+  $conn->close();
   ?>
 
   <br>
