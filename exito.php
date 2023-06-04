0870e02d5020db243cf4682794c8cac8f151d9c6
carlotagadel <124460669+carlotagadel@users.noreply.github.com>
2023-06-04 12:48:07 +0200

    cambio estilos

--- exito.php
+++ exito.php
@@ -19,47 +19,50 @@ if (!isset($_SESSION['acceso_autorizado']) || $_SESSION['acceso_autorizado'] !==
 </head>
 
 <body>
-  <h1>Registro exitoso</h1>
-  <p>¡Se ha registrado correctamente!</p>
+  <div class="container-body">
+      <h1>Registro exitoso</h1>
+      <p>¡Te has registrado correctamente!</p>
+    <?php
 
-  <?php
+    $conn = conectarDB();
 
-  $conn = conectarDB();
+    $sql = "SELECT * FROM usuarios_formulario";
+    $result = $conn->query($sql);
 
-  $sql = "SELECT * FROM usuarios_formulario";
-  $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      echo "<h2>Datos registrados</h2>";
+      echo "<div class=buttons><button class='mostrar-button' onclick='mostrarTabla()'>Mostrar Tabla</button>";
+      echo "<button class='ocultar-button' onclick='ocultarTabla()'>Ocultar Tabla</button></div>";
+      echo "<br/>";
+      echo "<table id='tabla' style='display: none;'>";
+      echo "<tr><th>Nombre</th><th>Apellido1</th><th>Apellido2</th><th>Email</th><th>Login</th><th>Contraseña</th></tr></div>";
 
-  if ($result->num_rows > 0) {
-    echo "<h2>Datos registrados</h2>";
-    echo "<button class='mostrar-button' onclick='mostrarTabla()'>Mostrar Tabla</button>";
-    echo "<button class='ocultar-button' onclick='ocultarTabla()'>Ocultar Tabla</button>";
-    echo "<table id='tabla' style='display: none;'>";
-    echo "<tr><th>Nombre</th><th>Apellido1</th><th>Apellido2</th><th>Email</th><th>Login</th><th>Contraseña</th></tr>";
+      while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["NOMBRE"] . "</td>";
+        echo "<td>" . $row["PRIMER_APELLIDO"] . "</td>";
+        echo "<td>" . $row["SEGUNDO_APELLIDO"] . "</td>";
+        echo "<td>" . $row["EMAIL"] . "</td>";
+        echo "<td>" . $row["LOG_IN"] . "</td>";
+        echo "<td>" . $row["CONTRASEÑA"] . "</td>";
+        echo "</tr>";
+      }
+      echo "</table>";
 
-    while ($row = $result->fetch_assoc()) {
-      echo "<tr>";
-      echo "<td>" . $row["NOMBRE"] . "</td>";
-      echo "<td>" . $row["PRIMER_APELLIDO"] . "</td>";
-      echo "<td>" . $row["SEGUNDO_APELLIDO"] . "</td>";
-      echo "<td>" . $row["EMAIL"] . "</td>";
-      echo "<td>" . $row["LOG_IN"] . "</td>";
-      echo "<td>" . $row["CONTRASEÑA"] . "</td>";
-      echo "</tr>";
+    } else {
+      echo "<p>No se encontraron datos registrados.</p>";
     }
-    echo "</table>";
 
-  } else {
-    echo "<p>No se encontraron datos registrados.</p>";
-  }
+    cerrarConexion($conn);
+    ?>
 
-  cerrarConexion($conn);
-  ?>
+    <br>
 
-  <br>
+    <a href="index.php">Volver al formulario</a>
 
-  <a href="index.php">Volver al formulario</a>
+    <script src="exito_script.js"></script>
 
-  <script src="exito_script.js"></script>
+  </div>
 </body>
 
 </html>
